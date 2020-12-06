a9ff00a514c2e5633af3c848ba30be9a4a5b207c
LeoGonzaga <sacredlotus05@gmail.com>
2020-12-05 23:17:26 -0300

    adicionando navegação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { GamesComponent } from './pages/games/games.component';
 
 const routes: Routes = [
+  { path: 'games/:name', component: GamesComponent },
   { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent },
 ];
